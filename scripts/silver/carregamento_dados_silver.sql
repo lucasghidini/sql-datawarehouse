31cd22ee6a000e4fbfda0fd0f976e61bac129636
Lucas Ghidini <lucasdemattos1306@gmail.com>
2025-11-05 23:57:30 -0300

    Carregando os dados na tabela silver.crm_sales_details

--- scripts/silver/carregamento_dados_silver.sql
+++ scripts/silver/carregamento_dados_silver.sql
@@ -1,7 +1,4 @@
-exec silver.load_data;
-
-create or alter procedure silver.load_data as
-
+CREATE OR ALTER PROCEDURE silver.load_silver AS
 begin
 	declare @start_time datetime, @end_time datetime, @comeco_time datetime, @final_time datetime;
 	begin try
@@ -90,6 +87,58 @@ begin
 			as date
 			) as prd_end_dt
 	from bronze.crm_prd_info;
+	set @end_time = getdate()
+	print '>>Tempo de carregamento: ' + cast(datediff(second, @start_time, @end_time) as nvarchar) + 'segundos';
+	print '>>-------------------';
+
+	-- carregando a tabela crm_sales_details
+	set @start_time = getdate();
+	print 'Truncate a tabela silver.crm_sales_details';
+	truncate table silver.crm_sales_details;
+	print 'Inserindo dados na: silver.crm_sales_details';
+	insert into silver.crm_sales_details (
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+	)
+	select
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	case
+		when sls_order_dt = 0 or len(cast(sls_order_dt as nvarchar)) != 8 then null
+		else cast(cast (sls_order_dt as nvarchar) as date)
+	end as sls_order_dt,
+	case
+		when sls_ship_dt = 0 or len(cast(sls_ship_dt as nvarchar)) != 8 then null
+		else cast(cast (sls_ship_dt as nvarchar) as date)
+	end as sls_ship_dt,
+	case
+		when sls_due_dt = 0 or len(cast(sls_due_dt as nvarchar)) != 8 then null
+		else cast(cast (sls_due_dt as nvarchar) as date)
+	end as sls_due_dt,
+	case
+		when sls_sales is null or sls_sales <=0 or sls_sales != sls_quantity * abs(sls_price)
+		then sls_quantity * abs(sls_price)
+		else sls_sales
+	end as sls_sales,
+	sls_quantity,
+	case
+		when sls_price is null or sls_price <= 0
+		then sls_sales / nullif(sls_quantity, 0)
+		else sls_price
+	end as sls_price
+	from bronze.crm_sales_details;
+	set @end_time = getdate();
+	print '>>Tempo de carregamento: ' + cast(datediff(second, @start_time, @end_time) as nvarchar) + 'segundos';
+	print '>>-------------------';
+	
 	end try
 	begin catch
 		print 'Erro ao carregar os dados da camada Bronze';
